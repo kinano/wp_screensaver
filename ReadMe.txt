14ac1e0ffc1048e132e3e38b2a2f95d280843def
Kinan Faham <k@theSilentCamera.com>
2019-01-13 21:55:30 -0500

    Update index.php and read me

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,14 +1,6 @@
 This repo has two files:
 1) index.php 
-This file will be used to call wordpress DB, fetch the image records and render them to Google/Picasa Screensaver clients.
-You can test that the repo works by browsing to the php file on your site. 
-Please make sure you read the comments in the code. 
-Also remember that you need both wordpress and nggallery plugin running on your site. 
-This file should be placed anywhere you want your screensaver to be visible to the public.
+Fetches images from nggallery DB tables in JSON format
 
 2) get_screen_saver_images.SQL 
-shows the DDL that will create the stored procedure(mySQL) responsible for fetching the picture records from your wordpress DB.
-You could modify the SP to limit the number of images it returns or the order of the pictures.
-If you are using other plugins to manage your wordpress pictures/galleries, you may need to modify the stored procedure a bit.
-
-If you have questions, please shoot me an email: k@theSilentCamera.com
+SP to make the DB call
--- index.php
+++ index.php
@@ -1,68 +1,30 @@
 <?php
-/*
- 	RSS2 Feed Template that will connect to a wordpress DB (with nggallery plugin installed). 
-	The page will get the picture records and display them using Google Picasa Screensaver format. You may see a working example on the following URL: 
-	http://theSilentCamera.com/screenSaver
-	@package theSilentCamera
- 	Remember that you have to output the XML with no whitespaces. Whitespaces will cause Picasa client to blow up...
-*/
-//	The images will be fetched from wordpress's DB. 
-//	We need to include the wp_config file. Change the following string based on your wordpress setup:
-require('/wp-config.php');
-//	Let's define our DB object using the constants in wp-config.php
-$mysqli  = new mysqli(DB_HOST,DB_USER, DB_PASSWORD,DB_NAME);
-//	Build the SQL query, We will call a dedicated SP that should be on your woredpress DB: 
-$sql = 'CALL get_screen_saver_images()';
-// 	Build the resultset
-$result = $mysqli->query($sql);
-// 	Define a local var that will hold wordpress URL(as seen in the blog settings and wp_options table):
-$url	=	site_url();
-//	Define a local variable for the title of the blog(e.g. the Silent Camera):
-$site_name	=	get_bloginfo();
-//	Get the blog tagline(description):
-$site_description	=	get_bloginfo ( 'description' );
-//	Render the xml according to the spec:
-echo ('<?xml version="1.0" encoding="utf-8"?>');
-?>
-<rss version="2.0"
-	 xmlns:media="http://search.yahoo.com/mrss/"
-	    xmlns:dc="http://purl.org/dc/elements/1.1/"
-	    xmlns:creativeCommons="http://cyber.law.harvard.edu/rss/creativeCommonsRssModule.html"
-	    xmlns:flickr="urn:flickr:" >
-<channel>
-	<title><?php echo($site_name);?></title>
-	<link><?php echo($url);?></link>
-	<description><?php echo($site_description);?></description>
-    <?php
-	//	Now, we need to get the individual pictures, their titles, descriptions and links:
-    while ($row = $result->fetch_object()) 
+	require('../blog/wp-config.php');
+	$mysqli  = new mysqli(SCREENSAVER_DB_HOST,SCREENSAVER_DB_USER, SCREENSAVER_DB_PASSWORD,SCREENSAVER_DB_NAME);
+	$nudes = 'false';
+	if(strtolower($_GET['nudes']) == 'true')
+	{
+		$nudes = 'true';
+	}
+	$sql = 'CALL get_screen_saver_images('.$nudes.')';
+	$result = $mysqli->query($sql);
+	$url	=	site_url();
+	$site_name	=	get_bloginfo();
+	$site_description	=	get_bloginfo ( 'description' );
+	header('Content-type: application/json');
+	$photos = array();
+    while ($row = $result->fetch_object())
     {
-		// 	The following var will hold the URL to the individual picture:
 		$link = $url.'/'.$row->path.'/'.$row->filename;
-		// 	The following var will hold the URL to the picture thumbnail(this is the default setting of nggallery plugin):
-		$thumb = $url.'/'.$row->path.'/thumbs/'.$row->filename;
-		//	Get the title of the picture
 		$title = html_entity_decode($row->title);
-		//	Get the description of the picture
-		$description = $row->description;
-		//	Render the picture:
-	?>
-    <item>
-        <pubDate><?php echo($row->imagedate);?></pubDate>
-        <lastBuildDate><?php echo($row->imagedate);?></lastBuildDate>
-        <generator><?php echo($url);?></generator>
-        <link><?php echo($link);?></link>
-        <description><![CDATA[<?php echo($description);?>]]></description>
-        <title><![CDATA[<?php echo($title);?>]]></title>
-        <media:content url="<?php echo($link);?>" type="image/jpeg"/> 
-	    <media:title><![CDATA[<?php echo($title);?>]]></media:title> 
-    	<media:description type="html"><![CDATA[<?php echo($description);?>]]></media:description> 
-   	 	<media:thumbnail url="<?php echo($thumb);?>" height="120" width="120" /> 
-    	<media:credit role="photographer"><?php echo($site_name.' | '.$site_description). ' | '. $row->imagedate; ?></media:credit> 
-    	<media:category scheme="urn:flickr:tags"></media:category> 
-    </item>
-	<?php
-	} //end the loop and close the xml output
-	?>
-</channel>
-</rss>
+		$photos[] = array(
+			'url' => "$link" ,
+			'owner' => "$site_name.' '.$site_description). ' '. $row->imagedate",
+			'title' => "$title"
+		);		
+	}
+	echo (json_encode(array(
+		"status" => "ok",
+		"photos" => $photos
+	)));
+?>
